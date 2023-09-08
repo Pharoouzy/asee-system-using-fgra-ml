9899643c14019ff034496ccf872bfc6cdd4838ab
Pharoouzy <pharoouzy@yahoo.com>
2023-09-08 04:36:05 +0100

    chore: complete api implementation

--- src/api/models/story_model.py
+++ src/api/models/story_model.py
@@ -1,69 +1,16 @@
-from pydantic import BaseModel, conint, confloat#, field_validator
+from pydantic import BaseModel, conint, field_validator
+from src.api.config import ISSUE_STATUSES
 
 class StoryModel(BaseModel):
-    # title: str
-    # description: str
-    # status: str
-    # priority: str
-    # ID: int
-    # Jira_ID: int
-    # Status_0: conint(ge=0, le=1)
-    # Status_1: conint(ge=0, le=1)
-    # Status_2: conint(ge=0, le=1)
-    # Status_3: conint(ge=0, le=1)
-    # Status_4: conint(ge=0, le=1)
-    # Status_5: conint(ge=0, le=1)
-    # Status_6: conint(ge=0, le=1)
-    # Resolution_0: conint(ge=0, le=1)
-    # Resolution_1: conint(ge=0, le=1)
-    # Resolution_2: conint(ge=0, le=1)
-    # Resolution_3: conint(ge=0, le=1)
-    # Resolution_4: conint(ge=0, le=1)
-    # Resolution_5: conint(ge=0, le=1)
-    # Story_Point: confloat(ge=0)
-    # In_Progress_Minutes: confloat(ge=0)
-    # Total_Effort_Minutes: confloat(ge=0)
-    # Resolution_Time_Minutes: confloat(ge=0)
-    # Title_Changed_After_Estimation: conint(ge=0, le=1)
-    # Description_Changed_After_Estimation: conint(ge=0, le=1)
-    # Story_Point_Changed_After_Estimation: conint(ge=0, le=1)
-    # Creator_ID: float
-    # Reporter_ID: float
-    # Assignee_ID: float
-    # Project_ID: int
-    # Sprint_ID: float
-    # Has_Resolution_Date: conint(ge=0, le=1)
-    # Time_To_Estimate_Minutes: int
-    # Title_Length: float
-    # Title_Unique_Words: float
-    # Description_Length: float
-    # Description_Unique_Words: float
-    # Description_Text_Length: float
-    # Description_Text_Unique_Words: float
-    # Description_Code_Length: float
-    # Description_Code_Unique_Words: float
-    Story_Point_Changed_After_Estimation: float
-    In_Progress_Minutes: float
-    Sprint_ID: float
-    Status_4: int
-    Time_To_Estimate_Minutes: int
-    Title_Changed_After_Estimation: int
-    Total_Effort_Minutes: float
-    Project_ID: int
-    Resolution_5: int
-    ID: int
+    title: str
+    description: str = 'No description'
+    description_code: str = 'No description code'
+    status: str = 'To Do'
 
-    # TODO: Add validation for story model
-    # @field_validator('story_type')
-    # def validate_story_type(self, story_type) -> str:
-    #     valid_types = ['bug', 'enhancement', 'task']
-    #     if story_type not in valid_types:
-    #         raise ValueError(f'Invalid story_type. Allowed values are {valid_types}')
-    #     return story_type
-    #
-    # @field_validator('story_status')
-    # def validate_story_status(self, story_status):
-    #     valid_types = ['To Do', 'Complete', 'In Progress', 'Done']
-    #     if story_status not in valid_types:
-    #         raise ValueError(f'Invalid story_status. Allowed values are {story_status}')
-    #     return story_status
+    @field_validator('status')
+    @classmethod
+    def validate_story_status(cls, status: str) -> str:
+        allowed_status = ', '.join(ISSUE_STATUSES)
+        if status not in ISSUE_STATUSES:
+            raise ValueError(f'Invalid story_status. Allowed values are [{allowed_status}]')
+        return status
--- src/api/routes/estimate.py
+++ src/api/routes/estimate.py
@@ -1,12 +1,22 @@
 import os
-import joblib
 from fastapi import APIRouter, HTTPException
 from src.api.models.story_model import StoryModel
 from datetime import datetime
 from src.utils import configs
 from joblib import load
+from src.api.utils.transformers import transform_request_data
+import numpy as np
 
-model = load(configs.general_configs()['artifact_paths']['model'])
+def load_model() -> object:
+    try:
+        project_root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', '..')
+        model_path: str = os.path.join(project_root_dir, configs.general_configs()['artifact_paths']['model'])
+        # Load the trained model
+        return load(model_path)
+    except Exception as e:
+        raise e
+
+model = load_model()
 
 router = APIRouter()
 
@@ -15,41 +25,23 @@ async def estimate(data: StoryModel):
     try:
         current_timestamp = datetime.now().isoformat()
         if model:
-            features = [
-                data.Description,
-                data.Title,
-                data.Sprint_ID,
-                data.Status,
-            ]
-            prediction = model.predict([features])
+            # Transform request data
+            transformed_data = transform_request_data(data)
+            X = [list(transformed_data.values())]
+            prediction = model.predict(X)
+
             if not prediction:
                 raise HTTPException(status_code=400, detail='Could not estimate story point.')
 
-            response = {
+            return {
                 'status': 'success',
                 'message': 'Effort estimation successful',
                 'timestamp': current_timestamp,
                 'model_version': '1.0.0',
-                'data': (prediction[0])
+                'estimation': np.round(prediction[0], 3)
             }
         else:
-            response =  {
-                'status': 'success',
-                'message': 'Effort estimation successful [test]',
-                'timestamp': current_timestamp,
-                'model_version': '1.0.0',
-                'data': { 'estimated_effort': 12.5 },
-            }
+            raise HTTPException(status_code=500, detail='Unable to estimate story point at this time, please try again later.')
 
-        return response
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-def load_trained_model() -> object:
-    try:
-        project_root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
-        model_path: str = os.path.join(project_root_dir, configs.general_configs()['artifact_paths']['model'])
-        # Load the trained model
-        return joblib.load(model_path)
-    except Exception as e:
-        raise e
+        raise HTTPException(status_code=500, detail=e.detail)
